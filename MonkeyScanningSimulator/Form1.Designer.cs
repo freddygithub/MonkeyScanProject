79ac9bd34c5f582f9c6d92e16e40379cebd7422e
Frederick Martinez <fmartinez@tompkinsinc.com>
2018-12-05 15:50:21 -0500

    changed a lot, multi threading now

--- MonkeyScanningSimulator/Form1.Designer.cs
+++ MonkeyScanningSimulator/Form1.Designer.cs
@@ -30,37 +30,32 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(69, 250);
+            this.button1.Location = new System.Drawing.Point(51, 330);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 46);
+            this.button1.Size = new System.Drawing.Size(187, 46);
             this.button1.TabIndex = 0;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(69, 103);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(150, 36);
-            this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(11, 46);
+            this.label1.Location = new System.Drawing.Point(11, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(276, 32);
             this.label1.TabIndex = 2;
@@ -71,27 +66,72 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(-11, -8);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(384, 428);
+            this.pictureBox1.Size = new System.Drawing.Size(315, 428);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
             // comboBox1
             // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(69, 169);
+            this.comboBox1.Location = new System.Drawing.Point(51, 173);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(150, 28);
+            this.comboBox1.Size = new System.Drawing.Size(187, 37);
             this.comboBox1.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.ItemHeight = 32;
+            this.comboBox2.Location = new System.Drawing.Point(51, 64);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(187, 40);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(34, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "How many times";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(68, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 32);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "How speed";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(51, 264);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(187, 37);
+            this.comboBox3.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(299, 409);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
@@ -105,10 +145,13 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox3;
     }
 }
 
--- MonkeyScanningSimulator/Form1.cs
+++ MonkeyScanningSimulator/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,11 +14,33 @@ namespace MonkeyScanningSimulator
 {
     public partial class Form1 : Form
     {
+        Thread thread; int stopFlag = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+            readPast();
+            mySetup();
+            TopMost = true;
+        }
+
+        public void mySetup()
+        {
+            comboBox3.Enabled = true;
+            comboBox2.Enabled = true;
+            comboBox1.Enabled = true;
+
             label1.Parent = pictureBox1;
             label1.BackColor = Color.Transparent;
+            label2.Parent = pictureBox1;
+            label2.BackColor = Color.Transparent;
+            label3.Parent = pictureBox1;
+            label3.BackColor = Color.Transparent;
+
+            comboBox3.Items.Add(1);
+            comboBox3.Items.Add(3);
+            comboBox3.Items.Add(5);
 
             comboBox1.Items.Add(10);
             comboBox1.Items.Add(50);
@@ -27,20 +51,87 @@ namespace MonkeyScanningSimulator
             MinimizeBox = false;
         }
 
-        public void typing(string text, string times)
+        public void readPast()
         {
-            int time = int.Parse(times);
+            try
+            {
+                using (var tsw = new StreamReader(Environment.CurrentDirectory + @"\save.txt"))
+                {
+                    string line;
+                    while ((line = tsw.ReadLine()) != null)
+                    {
+                        comboBox2.Items.Add(line);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                return;
+            }
+        }
+
+        public void typing(string text, string times, string speed)
+        {
+            int zoom = 0;
+            int time = 0;
+
+            try
+            {
+                time = int.Parse(times);
+                zoom = int.Parse((1000 * double.Parse(speed)).ToString());
+            }
+            catch
+            {
+                MessageBox.Show("You not good typing",
+                                    "Error: could not parse number you entered", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        
+            if (zoom <= 0)
+                zoom = 5000;
 
             for (int i = 0; i < time; i++)
-                foreach(char a in text)
-                    SendKeys.Send("" + a);
-            
-            SendKeys.Send("{ENTER}");
+            {
+                if (stopFlag == 1)
+                    break;
+
+                foreach (char a in text)
+                    SendKeys.SendWait("" + a);
+
+                SendKeys.SendWait("{ENTER}");
+
+                Thread.Sleep(zoom);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            typing(textBox1.Text, comboBox1.Text);
+            label1.Focus();
+
+            if(thread !=null && thread.IsAlive)
+            {
+                stopFlag = 1;
+                thread.Abort();
+                return;
+            }
+
+            string param1 = comboBox2.Text;
+            string param2 = comboBox1.Text;
+            string param3 = comboBox3.Text;
+
+            stopFlag = 0;
+            thread = new Thread(() => typing(param1, param2, param3));
+            thread.Start();
+            
+            using (var tsw = new StreamWriter(Environment.CurrentDirectory + @"\save.txt"))
+            {
+                tsw.WriteLine(comboBox2.Text);
+            }
+        }
+
+        private void Form1_Closing()
+        {
+
         }
     }
 }
